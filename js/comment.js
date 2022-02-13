101a5d253f66028f5ff204a7e2d0ac3207a607c8
Mejbah <mejba.s@gmail.com>
2022-02-14 03:38:13 +0600

    change for shortcut in appendChild

--- js/comment.js
+++ js/comment.js
@@ -1,11 +1,12 @@
 document.getElementById('submit-button').addEventListener('click', function(){
     const commentBox = document.getElementById('new-comment');
 
+    // create comment paragraph
     const newComment = document.createElement('p');
     newComment.innerText = commentBox.value;
 
-    const commentContainer = document.getElementById('comment-container');
-    commentContainer.appendChild(newComment);
+    // append the comment
+    const commentContainer = document.getElementById('comment-container').appendChild(newComment);
 
     commentBox.value = '';
 
